1e06fef3c3810d18b2b8686cd2d194b1fe549ea7
GBAGOUEHI GUEASSA ANGE ANDRE <angeandregueassa@gmail.com>
2025-10-01 19:13:30 +0200

    change

--- src/app/components/aside-nav/aside-nav.component.ts
+++ src/app/components/aside-nav/aside-nav.component.ts
@@ -12,6 +12,7 @@ export class AsideNavComponent implements OnInit {
 navs_data: Items[] = nav_items
 auths_data: Items[] = auth_items
 @Output() close: EventEmitter<string> = new EventEmitter<string> ()
+totalProduits: any;
 
 
 constructor(){}
